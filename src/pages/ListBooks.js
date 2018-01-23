9380de3e815d090bdc417aaa91f94963bc5af9c8
Leonardo Silva <s.leosoares@gmail.com>
2018-01-23 12:47:14 -0200

    refactor: Moved api file for new folder utils

--- src/pages/ListBooks.js
+++ src/pages/ListBooks.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import * as BooksAPI from '../BooksAPI'
+import * as BooksAPI from '../utils/BooksAPI'
 
 import Loader from '../components/Loader'
 import BookShelf from '../components/BookShelf'
